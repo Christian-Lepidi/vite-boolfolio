3792f8ac5448f99ed1aa6cf0d5a25d27932a49a1
Christian-Lepidi <christianlepidi@gmail.com>
2024-04-12 19:04:08 +0200

    add. stamp on page

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,34 @@ import axios from "axios";
 
 export default {
   data() {
-    return {};
+    return {
+      title: "Boolpress",
+      projects: [],
+    };
   },
 
   created() {
     axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
-      console.log(response);
+      this.projects = response.data;
     });
   },
 };
 </script>
 
-<template></template>
+<template>
+  <h1>{{ title }}</h1>
+  <div v-for="project in projects">
+    <ul>
+      <li><strong>Titolo:</strong> {{ project.title }}</li>
+      <li><strong>Descrizione:</strong> {{ project.description }}</li>
+      <li>
+        <strong>Data di pubblicazione:</strong>
+        {{ project.date_of_publication }}
+      </li>
+      <hr />
+    </ul>
+  </div>
+</template>
 
 <style lang="scss">
 @use "/src/scss/general.scss";
