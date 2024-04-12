14e0bc7c1417e49cbca3b793b04db3ec6c4b4244
Christian-Lepidi <christianlepidi@gmail.com>
2024-04-12 17:53:05 +0200

    add. vite+vue with scss and axios

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {};
+  },
+
+  created() {
+    axios.get("http://127.0.0.1:8000").then((response) => {});
+  },
+};
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use "/src/scss/general.scss";
+</style>
